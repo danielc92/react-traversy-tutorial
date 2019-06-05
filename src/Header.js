6f3c3d22e57f781a0aa96320352b631f85685476
Daniel Corcoran <danielc.92@outlook.com>
2019-06-05 11:35:28 +1000

    Ran 'npm run build'

--- src/Header.js
+++ src/Header.js
@@ -8,7 +8,7 @@ const linkButton = "button is-small is-outlined is-link";
 function Header () {
     return (
         <React.Fragment>
-        <section className="navbar">
+        <section className="navbar" style={{marginBottom:'1.6rem'}}>
             <div className="content">
             <h3>Header Component</h3>
             <p>Pariatur sureincididunt ad sit consectetur eu ex non exercitation dolor tempor magna. Proident elit adipisicing et eiusmod ut sit ea in cupidatat commodo qui id. Et laborum do irure laboris magna voluptate voluptate culpa sit culpa sit consequat reprehenderit sint. Consectetur eiusmod cupidatat qui sint elit est deserunt dolore non excepteur veniam ex adipisicing commodo. Sunt velit esse duis qui elit voluptate minim aliquip non irure officia aliqua adipisicing.</p>
