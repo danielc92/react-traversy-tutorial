8c1634735f41c4b27ca132ce9ef93c8b59a6e369
Daniel Corcoran <danielc.92@outlook.com>
2019-06-05 11:16:28 +1000

    Added <Link>s from react-router-dom

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 // This is a functional component
 // These should be used when only a render is called
+const linkButton = "button is-small is-outlined is-link";
+
 function Header () {
     return (
         <React.Fragment>
         <section className="navbar">
             <div className="content">
             <h3>Header Component</h3>
-            <p>Pariatur sint incididunt ut officia eiusmod irure sint ex incididunt. Et veniam incididunt ad sit consectetur eu ex non exercitation dolor tempor magna. Proident elit adipisicing et eiusmod ut sit ea in cupidatat commodo qui id. Et laborum do irure laboris magna voluptate voluptate culpa sit culpa sit consequat reprehenderit sint. Consectetur eiusmod cupidatat qui sint elit est deserunt dolore non excepteur veniam ex adipisicing commodo. Sunt velit esse duis qui elit voluptate minim aliquip non irure officia aliqua adipisicing.</p>
+            <p>Pariatur sureincididunt ad sit consectetur eu ex non exercitation dolor tempor magna. Proident elit adipisicing et eiusmod ut sit ea in cupidatat commodo qui id. Et laborum do irure laboris magna voluptate voluptate culpa sit culpa sit consequat reprehenderit sint. Consectetur eiusmod cupidatat qui sint elit est deserunt dolore non excepteur veniam ex adipisicing commodo. Sunt velit esse duis qui elit voluptate minim aliquip non irure officia aliqua adipisicing.</p>
             </div>
+            <div className="buttons">
+                <Link className={linkButton} to="/">Visit the Main page</Link>
+                <Link className={linkButton} to="/about">Visit the About page</Link>
+            </div>
+            
         </section>
-        <code>Hello world</code>
         </React.Fragment>
     )
 
