bf6995c450f785d13a553dd2550a58a8e6df7211
Daniel Corcoran <danielc.92@outlook.com>
2019-06-05 11:37:47 +1000

    Removed build from gitignore

---
+++ build/precache-manifest.9f39b3eab916ba0d0f687add2b9fd640.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ed4e02a4f8edbb236de33987252c98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b496331fcd0137c09e3",
+    "url": "/static/js/2.b54c6c86.chunk.js"
+  },
+  {
+    "revision": "c61aea44f9d9aa4fdf87",
+    "url": "/static/js/main.e25740f8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
